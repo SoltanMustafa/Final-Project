7dc23f26ce0ceb8021ee419c9f898d7ee8449b43
SoltanMustafa <soltan.mustafali290@gmail.com>
2024-01-18 20:42:34 +0400

    MainWrapperChange

--- FinalProject/src/CounterApp.jsx
+++ FinalProject/src/CounterApp.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import MainRouter from "./router";
-import { useSelector } from "react-redux";
 
 export default function CounterApp() {
-  const sideMenuActive = useSelector((state) => state.sideMenu.sideMenuActive);
-
   return (
     <>
-      <div className={`wrapper ${sideMenuActive ? "wrapper-left-250px" : ""}`}>
-        <MainRouter />
-      </div>
+      <MainRouter />
     </>
   );
 }
--- FinalProject/src/layouts/Auth/AuthLayouts.jsx
+++ FinalProject/src/layouts/Auth/AuthLayouts.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function AuthLayouts() {
+  const sideMenuActive = useSelector((state) => state.sideMenu.sideMenuActive);
   return (
     <>
-      <Outlet />
+      <div className={`wrapper ${sideMenuActive ? "wrapper-left-250px" : ""}`}>
+        <Outlet />
+      </div>
     </>
   );
 }
